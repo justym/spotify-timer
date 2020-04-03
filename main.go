add984b56f8d50753ba6c5a82080df6124ec3cc7
justym <40595126+justym@users.noreply.github.com>
2020-04-04 08:39:42 +0900

    fix

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/justym/spotify-timer/oauth2"
+	own "github.com/justym/spotify-timer/oauth2"
 	"fmt"
 )
 
 func main(){
-	fmt.Println("Spotify-timer")
+	fmt.Println(own.Config.RedirectURL)
 }
