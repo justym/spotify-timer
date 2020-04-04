a38ec4f957e658eac2a420bd92b3fcca6d2ef075
justym <40595126+justym@users.noreply.github.com>
2020-04-04 09:04:01 +0900

    fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"flag"
 	"net/http"
 
@@ -16,12 +15,6 @@ func main() {
 	http.HandleFunc("/", controller.Home)
 	http.HandleFunc("/auth", controller.Authorize)
 	http.ListenAndServe(port, nil)
-=======
-	own "github.com/justym/spotify-timer/oauth2"
-	"fmt"
 )
 
-func main(){
-	fmt.Println(own.Config.RedirectURL)
->>>>>>> 1c79828c5b5d8496d164401906da99c4f9e380b5
-}
+
