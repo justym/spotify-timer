77ff1d778262481b39cc838c0d72d155b95f5563
justym <40595126+justym@users.noreply.github.com>
2020-04-04 15:18:23 +0900

    Update pause.go

--- player/pause.go
+++ player/pause.go
@@ -9,13 +9,13 @@ import (
 const pauseURL = "https://api.spotify.com/v1/me/player/pause"
 
 //Pause pauses playback
-func Pause(token string, client *http.Client) error {
+func Pause(accessToken string, client *http.Client) error {
 	req, err := http.NewRequest(http.MethodPut, pauseURL, nil)
 	if err != nil {
 		log.Println(err)
 		return errors.New("Failed to create New Request in PauseOpt")
 	}
-	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Authorization", "Bearer "+ accessToken)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
