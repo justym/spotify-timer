b82dc9227ee7764ce4fee7bcc25ca8608affc985
justym <mm6mm0mm3mm@gmail.com>
2020-04-06 13:22:39 +0900

    add time.Sleep (timer) still ugly prototype

--- cmd/spotify-timer/main.go
+++ cmd/spotify-timer/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/justym/spotify-timer/cmd/spotify-timer/server"
 	o "github.com/justym/spotify-timer/lib/oauth2"
@@ -25,16 +26,17 @@ func main() {
 	http.HandleFunc("/callback", server.Authorize)
 
 	var client *http.Client
-	go func() {
+	go func(t time.Duration) {
 		url := o.GetRedirectURL()
 		fmt.Println("Please Log In to Spotify from this URL: ", url)
 		token := o.AccessToken
 		client = <-o.Ch
+		time.Sleep(t)
 		err := player.Pause(token, client)
 		if err != nil {
 			log.Fatal(err)
 		}
-	}()
+	}(10 * time.Second)
 
 	http.ListenAndServe(port, nil)
 }
