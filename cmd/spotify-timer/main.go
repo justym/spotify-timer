2f400319b44815f6b808fd7ee9a1b4203a38c517
justym <mm6mm0mm3mm@gmail.com>
2020-04-11 12:38:42 +0900

    flag

--- cmd/spotify-timer/main.go
+++ cmd/spotify-timer/main.go
@@ -13,30 +13,33 @@ import (
 )
 
 var port string
+var m int
 
 func init() {
-	flag.StringVar(&port, "port", ":8080", "Default is :8080")
+	flag.StringVar(&port, "port", ":8080", "[Default] :8080")
+	flag.IntVar(&m, "m", 15, "[Default] 15minute")
 }
 
 func main() {
-
 	flag.Parse()
 
-	http.HandleFunc("/", server.Home)
 	http.HandleFunc("/callback", server.Authorize)
 
-	var client *http.Client
-	go func(t time.Duration) {
-		url := o.GetRedirectURL()
-		fmt.Println("Please Log In to Spotify from this URL: ", url)
-		token := o.AccessToken
-		client = <-o.Ch
-		time.Sleep(t)
-		err := player.Pause(token, client)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(10 * time.Second)
+	tstop := time.Duration(m) * time.Minute
+	go pause(tstop)
 
 	http.ListenAndServe(port, nil)
+}
+
+func pause(d time.Duration) {
+	url := o.GetRedirectURL()
+	fmt.Println("Please Log in to spotify from this URL: ", url)
+	token := o.AccessToken
+	client := <-o.Ch
+	t := time.NewTimer(d)
+	<-t.C
+	err := player.Pause(token, client)
+	if err != nil {
+		log.Println(err)
+	}
 }
--- cmd/spotify-timer/server/handler.go
+++ cmd/spotify-timer/server/handler.go
@@ -21,14 +21,5 @@ func Authorize(w http.ResponseWriter, r *http.Request) {
 	}
 	o.AccessToken = token.AccessToken
 	o.Ch <- o.Config.Client(context.Background(), token)
-
-	fmt.Fprintf(w, "Authenticate Handler")
-	log.Println("Got Request for /auth")
-}
-
-//Home is http handler for Home
-func Home(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Home Handler")
-	log.Println("Got Request for /")
+	fmt.Fprintf(w, "Success\n")
 }
