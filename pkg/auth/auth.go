a7fca866c7df6d9ab3ab739a5a5717db6476ace5
justym <mm6mm0mm3mm@gmail.com>
2020-05-01 00:46:28 +0900

    reshape auth package

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -2,26 +2,20 @@ package auth
 
 import (
 	"log"
-	"os"
 
 	"github.com/justym/spotify-timer/pkg/config"
 	a2ns "github.com/nmrshll/oauth2-noserver"
 )
-
-var authorizedClient *a2ns.AuthorizedClient
 
 func NewClinet() *a2ns.AuthorizedClient {
 	conf := config.NewConfig()
 	if conf == nil {
 		log.Fatal("config is nil")
 	}
-	if authorizedClient == nil {
-		authorizedClient, err := a2ns.AuthenticateUser(conf)
-		if err != nil {
-			log.Println(err)
-			os.Exit(1)
-		}
-		return authorizedClient
+
+	authorizedClient, err := a2ns.AuthenticateUser(conf)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	return authorizedClient
